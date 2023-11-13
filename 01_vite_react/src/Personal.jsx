02e8b58ca79a83c6216ef6385dac34bc6e581c75
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-11-13 18:34:19 +0530

    Try to add our personal file..

---
+++ 01_vite_react/src/Personal.jsx
@@ -0,0 +1,11 @@
+function Personal(){
+    return(
+        <>
+        <h1>This is our personal React code...</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti corporis at mollitia exercitationem sint enim.
+        </p>
+        </>
+    )
+}
+export default Personal
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Personal from './personal.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Personal />
   </React.StrictMode>,
 )
