4b70cdf7c79c0ec331115db91ec877bd78c68a4f
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-11-14 12:09:46 +0530

    Build a function inside main.jsx

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Personal from './personal.jsx'
 
+function CustomComponent(){
+  return(
+    <h2>Our Custom Component</h2>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Personal />
+    { /* <CustomComponent /> */}
   </React.StrictMode>,
 )
