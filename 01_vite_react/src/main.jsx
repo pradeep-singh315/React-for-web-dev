c0c5cf7ea0163bc829c761b1e3cd20d5c2d253ae
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-11-14 12:23:06 +0530

    our custom tag ---- a

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Personal from './personal.jsx'
 
-function CustomComponent(){
-  return(
-    <h2>Our Custom Component</h2>
-  )
-}
+// function CustomComponent(){
+//   return(
+//     <h2>Our Custom Component</h2>
+//   )
+// }
+
+const anotherElement = (
+  <a href='https://www.google.com' target='_blank'>Click here to Visit Google </a>
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <Personal />
-    { /* <CustomComponent /> */}
-  </React.StrictMode>,
+     
+    anotherElement
 )
